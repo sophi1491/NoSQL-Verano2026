974caaf91c6d0535edeb702730757e2fe32d93a8
sophi1491 <sogonzalezza@ittepic.edu.mx>
2026-07-21 09:39:44 -0700

    Actividad 3: Api Netflix, get,put, post y delete

--- 23400553_Gonzalez_Zapete_Sophia/unidad_4/apiBase/index.js
+++ 23400553_Gonzalez_Zapete_Sophia/unidad_4/apiBase/index.js
@@ -2,9 +2,67 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose'); 
 
 
-app.use(morgan('combined'));
+app.use(morgan('dev'));
+
+mongoose.connect("mongodb:/127.0.0.1:27017/escuela")
+.then(() => {
+    console.log("Conectado correctamente a MongoDB");
+})
+.catch((error) => {
+    console.error("Error al conectar con MongoDB:", error);
+})
+
+const alumnoShema = new mongoose.Schema(
+    {
+        nombre:{type: String, required: true, trim: true},
+        carrera:{type: String, required: true, trim: true},
+        semestre:{type: String, required: true, min: 1}
+    },{
+        timestamps: true
+    }
+);
+
+const Alumno = mongoose.model("Alumno",alumnoShema,"alumnos");
+
+app.get("/alumno", async (req, res) => {
+    try{
+        const alumnos = await Alumno.find();
+        res.json(alumnos);
+    }catch{
+        res.status(500).json({
+                mensaje: "Error al obtener los alumnos",
+                error: error
+        })
+    }
+});
+
+
+app.put("/alumno/:id", async (req, res) => {
+    try{
+        const alumnos = await Alumno.find();
+        res.json(alumnos);
+    }catch{
+        res.status(500).json({
+                mensaje: "Error al obtener los alumnos",
+                error: error
+        })
+    }
+});
+
+app.post("/alumno", async (req, res) => {
+    try{
+        const alumnos = await Alumno.find();
+        res.json(alumnos);
+    }catch{
+        res.status(500).json({
+                mensaje: "Error al obtener los alumnos",
+                error: error
+        })
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('¡Hola, mundo!');
@@ -57,7 +115,7 @@ app.get("/par/:numero", (req, res) => {
 });
 
 
-app.get("edad/:edad",(req,res) => {
+app.get("/edad/:edad",(req,res) => {
     const { edad } = req.params;
     const esMayor = parseInt(edad) >= 18;
     res.send(`<p> Eres ${esMayor ? 'Mayor' : 'Menor'} de edad.</p>`)
@@ -65,12 +123,32 @@ app.get("edad/:edad",(req,res) => {
 });
 
 
+app.delete("/alumno/id:", (req,res) => {
+try{    
+    const id = Number(req.params.id);
+    const alumno = Alumno.findById(id);
+    if(!alumno){
+        return res.status(404).json({
+                mensaje: "Alumno no encontrado"
+        });
+    }
+    res.json(alumno);
+}catch{
+    return res.status(500).json({
+                mensaje: "Error al encontrar al alumno"
+        });
+
+}
+    
+
+});
+
 
 /*app.get("/aleatorio", (req, res) => {
     const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
     res.send(`<p>El número aleatorio generado es: ${numeroAleatorio}</p>`);
 });*/
-/*
+
 app.listen(port, () => {
     console.log(`Servidor iniciado en http://localhost:${port}`);
-});*/
+});
---
+++ 23400553_Gonzalez_Zapete_Sophia/unidad_4/apiNetflix/index.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose')
+
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+mongoose.connect("mongodb+srv://grupo:grupo@servidorprueba.ygegryf.mongodb.net/netflix")
+.then(() => {
+    console.log("Conectado correctamente a MongoDB");
+})
+.catch((error)=>{
+    console.log("Error al conectar con MongoDB: ",error);
+})
+
+
+
+const Pelicula = mongoose.model("Pelicula", peliculaSchema, "Peliculas");
+
+app.get("/peliculas", async (req,res) =>{
+    try{
+        const peliculas = await Pelicula.find();
+        res.json(peliculas);
+    } catch (error) {
+        res.status(500).json({
+            mensaje: "Error al obtener las películas",
+            error: error
+        });
+    }
+})
+
+app.get("/peliculas/:id", async (req,res) => {
+    try {
+        const id = req.params.id;
+        const pelicula = await Pelicula.findById(id);
+
+        if (!pelicula) {
+            return res.status(404).json({
+                mensaje: "Película no encontrada"
+            })  
+        }
+        res.json(pelicula);
+    } catch (error) {
+        res.status(500).json({
+            mensaje: "Error al obtener película",
+            error: error
+        });
+    }
+}) 
+
+app.post("/peliculas", async (req,res) => {
+    try{
+        const { titulo, genero, año,duracion,idioma,calificacion,nc  } = req.body;
+        if (!titulo || !genero || !año || !duracion || !idioma || !calificacion || !nc) {
+            return res.status(400).json({
+                mensaje: "Faltan datos de la película"
+            });
+        }
+        const nuevaPelicula = new Pelicula({
+            titulo, genero, año,duracion,idioma,calificacion,nc
+        });
+
+        const peliculaGuardada = await nuevaPelicula.save();
+
+        res.status(201).json({
+            mensaje: "Película registrada correctamente",
+            pelicula: peliculaGuardada
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            mensaje: "Error al obtener película",
+            error: error
+        });
+    }
+})
+
+app.put("/peliculas/:id", async (req,res) => {
+    try {
+        const id = req.params.id;
+        const { titulo, genero, año,duracion,idioma,calificacion,nc  } = req.body;
+        if (!titulo || !genero || !año || !duracion || !idioma || !calificacion || !nc) {
+            return res.status(400).json({
+                mensaje: "Faltan datos de la película"
+            });
+        }
+
+        const peliculaActualizada = await Pelicula.findByIdAndUpdate(
+            id, 
+            { titulo, genero, año,duracion,idioma,calificacion,nc }, 
+            { new: true, runValidators: true }
+        );
+        
+        if (!peliculaActualizada) {
+            return res.status(404).json({
+                mensaje: "Película no encontrada"
+            });
+        }
+
+        res.json({
+            mensaje: "Película actualizada correctamente",
+            pelicula: peliculaActualizada
+        });
+    } catch (error) {
+        res.status(500).json({
+            mensaje: "Error al obtener película",
+            error: error
+        });
+    }
+})
+
+app.delete("/peliculas/:id", async (req,res) => {
+    try {
+        const id = req.params.id;
+        const peliculaEliminada = await Pelicula.findByIdAndDelete(id);
+
+        if (!peliculaEliminada) {
+            return res.status(404).json({
+                mensaje: "Película no encontrada"
+            });
+        }
+
+        res.json({
+            mensaje: "Película eliminada correctamente",
+            pelicula: peliculaEliminada 
+        });
+    } catch (error) {
+        res.status(500).json({
+            mensaje: "Error al eliminar película",
+            error: error
+        });
+    }
+});
+
+app.listen(port, () => {
+    console.log("Servidor iniciado en http://localhost:"+port);
+})
