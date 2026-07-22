b063ad678cf59944af99b0d96faed3381974898e
sophi1491 <sogonzalezza@ittepic.edu.mx>
2026-07-22 08:32:42 -0700

    Feat: diseño de intefaz para post

---
+++ 23400553_Gonzalez_Zapete_Sophia/unidad_4/apiNetflix/api.js
@@ -0,0 +1,35 @@
+api.js
+
+const API_URL = "URL_VERCEL";
+
+// Obtener películas
+async function obtenerPeliculas() {
+
+    const respuesta = await fetch(`${API_URL}/peliculas`);
+
+    if (!respuesta.ok) {
+        throw new Error("Error al consultar las películas");
+    }
+
+    return await respuesta.json();
+
+}
+
+// Agregar película
+async function agregarPelicula(pelicula) {
+
+    const respuesta = await fetch(`${API_URL}/peliculas`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pelicula)
+    });
+
+    if (!respuesta.ok) {
+        throw new Error("Error al guardar la película");
+    }
+
+    return await respuesta.json();
+
+}
